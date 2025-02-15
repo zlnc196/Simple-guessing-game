d95322049084e751890b6b1090bccef00f10761e
zlnc <collinsolafusi@gmail.com>
2025-02-15 19:33:46 +0000

    added a tracker of number of (valid) guesses to reach correct answer

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -5,6 +5,7 @@ use rand::{rng, Rng};
 fn main() {
     
     let mut rng = rand::rng();
+    let mut guessNumber: u32 = 0;
 
     let answer = rng.random_range(1..=100);
 
@@ -14,7 +15,7 @@ fn main() {
         println!("Guess the number");
         let mut userGuessStr = String::new();
         io::stdin().read_line(&mut userGuessStr).expect("Error in entering answer");
-        
+
         let mut userGuessInt;
         match userGuessStr.trim().parse() {
             Ok(num) => userGuessInt = num,
@@ -24,12 +25,17 @@ fn main() {
         if userGuessInt == answer {
             isCorrect = true;
             println!("{} is the correct answer", userGuessInt);
+            println!("It took you {} guesses to reach the correct answer", guessNumber);
         } else if userGuessInt > answer {
             println!("Answer is smaller");
+            guessNumber += 1;
         } else if answer > userGuessInt {
             println!("Answer is larger");
+            guessNumber += 1;
         }
 
     }
+
+
 }
 
